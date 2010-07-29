91f22a807106def9b0fb6d95091d0f4de7773996
cedric.horner <cedric.horner@79222922-03e5-11de-9d1d-173cc1d06e3c>
2010-07-29 10:25:03 +0000

    Syreen homeworld comm Implementation take 2

--- trunk/sc2/src/sc2code/comm/syreen/igfxres.h
+++ trunk/sc2/src/sc2code/comm/syreen/igfxres.h
@@ -2,4 +2,4 @@
    should not be edited directly.  Modify the master resource list
    instead and regenerate. */
 
-#define SYREEN_PMAP_ANIM "comm.syreen.graphics"
+#define SYREEN_PMAP_ANIM "comm.syreenhome.graphics"
