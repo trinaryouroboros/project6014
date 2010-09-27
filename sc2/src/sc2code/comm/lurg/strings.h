012c9daa4fa046538727381c13a3d5aa74ef99cc
ejrh00 <ejrh00@79222922-03e5-11de-9d1d-173cc1d06e3c>
2010-09-27 11:32:08 +0000

    Basic Lurg conversation.  There are lots more strings in the text file that need to be threaded into the conversation.

--- sc2/src/sc2code/comm/lurg/strings.h
+++ sc2/src/sc2code/comm/lurg/strings.h
@@ -22,8 +22,39 @@
 enum
 {
 	NULL_PHRASE,
-	LURG_HOME_HELLO_1,
-	LURG_SPACE_HELLO_1
+    LURG_HOME_HELLO_1,
+    LURG_SPACE_HELLO_1,
+    we_mean_no_harm,
+    distress_call,
+    suck_vacuum,
+    CANT_LEAVE,
+    DONT_INSULT_US,
+    let_us_pass,
+    can_we_pass,
+    tell_us_stuff,
+    CANT_PASS,
+    ASK_YOUR_QUESTIONS,
+    tellus_species,
+    tellus_ship,
+    tellus_shielded,
+    we_are_peaceful,
+    dont_tellus,
+    URQUAN_ALLIANCE,
+    NOT_PEACEFUL,
+    OUR_SPECIES,
+    OUR_SHIP,
+    you_look_familiar, 
+    YOU_FEEL_US,
+    alien_stalkers,
+    creepy,
+    big_imagination,
+    WE_KNOW_ALL,
+    we_kick_ass,
+    we_are_powerful,
+    EASILY_DESTROYED,
+    last_chance,
+    lets_fight,
+    GOODBYE
 };
 
 #endif /* _STRINGS_H */
