eaf336119fc255a5a4813ae614e010e988175eb2
Jaakko.Markus.Seppala <Jaakko.Markus.Seppala@79222922-03e5-11de-9d1d-173cc1d06e3c>
2010-09-28 14:53:49 +0000

    Added chmmr explorer ditty and lurg special graphics to resources.csv in tools/resmap. This way the gen_resfiles.py utility can be used without wiping the ditty and lurgspecial related stuff in uqm.rmp

--- sc2/src/sc2code/ships/sis_ship/imusicre.h
+++ sc2/src/sc2code/ships/sis_ship/imusicre.h
@@ -2,4 +2,5 @@
    should not be edited directly.  Modify the master resource list
    instead and regenerate. */
 
+#define EXP_VICTORY_SONG "ship.explorer.ditty"
 #define SIS_VICTORY_SONG "ship.sis_ship.ditty"
