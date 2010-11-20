cc5d42c6584493141f83487b8e200b3a42eae746
OrzInvasion <OrzInvasion@gmail.com>
2010-11-20 03:40:59 +0000

    [MSVC Compiler Fix]
    * Shoved something into wspiapiwrap.h to get MSVC to compile. If this breaks anything please revert this change and send me hatemail.

--- sc2/src/sc2code/libs/network/wspiapiwrap.h
+++ sc2/src/sc2code/libs/network/wspiapiwrap.h
@@ -16,6 +16,11 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+// Shiver: Added this part to help MSVC compile.
+#ifdef _MSC_VER
+#include <WSPiApi.h>
+#endif
+
 #ifndef _WSPIAPIWRAP_H
 #define _WSPIAPIWRAP_H
 
