067fcf663fc00a626741974aa81385e9997c82c8
Jaakko.Markus.Seppala <Jaakko.Markus.Seppala@79222922-03e5-11de-9d1d-173cc1d06e3c>
2011-11-09 10:31:30 +0000

    Corrected foonfoon centerings for lo-res.

--- trunk/sc2/src/uqm/ships/foonfoon/igfxres.h
+++ trunk/sc2/src/uqm/ships/foonfoon/igfxres.h
@@ -2,15 +2,15 @@
    should not be edited directly.  Modify the master resource list
    instead and regenerate. */
 
+#define FOONFOON_BEAM_BIG_MASK_PMAP_ANIM "ship.foonfoon.graphics.beam.large"
+#define FOONFOON_BEAM_MED_MASK_PMAP_ANIM "ship.foonfoon.graphics.beam.medium"
+#define FOONFOON_BEAM_SML_MASK_PMAP_ANIM "ship.foonfoon.graphics.beam.small"
 #define FOONFOON_BIG_MASK_PMAP_ANIM "ship.foonfoon.graphics.typhoon.large"
 #define FOONFOON_CAPTAIN_MASK_PMAP_ANIM "ship.foonfoon.graphics.captain"
 #define FOONFOON_ICON_MASK_PMAP_ANIM "ship.foonfoon.icons"
 #define FOONFOON_MED_MASK_PMAP_ANIM "ship.foonfoon.graphics.typhoon.medium"
 #define FOONFOON_MICON_MASK_PMAP_ANIM "ship.foonfoon.meleeicons"
-#define FOONFOON_PRIMARY_BIG_MASK_PMAP_ANIM "ship.foonfoon.graphics.ffprimary.large"
-#define FOONFOON_PRIMARY_MED_MASK_PMAP_ANIM "ship.foonfoon.graphics.ffprimary.medium"
-#define FOONFOON_PRIMARY_SML_MASK_PMAP_ANIM "ship.foonfoon.graphics.ffprimary.small"
-#define FOONFOON_SECONDARY_BIG_MASK_PMAP_ANIM "ship.foonfoon.graphics.ffsecondary.large"
-#define FOONFOON_SECONDARY_MED_MASK_PMAP_ANIM "ship.foonfoon.graphics.ffsecondary.medium"
-#define FOONFOON_SECONDARY_SML_MASK_PMAP_ANIM "ship.foonfoon.graphics.ffsecondary.small"
+#define FOONFOON_SABRE_BIG_MASK_PMAP_ANIM "ship.foonfoon.graphics.sabre.large"
+#define FOONFOON_SABRE_MED_MASK_PMAP_ANIM "ship.foonfoon.graphics.sabre.medium"
+#define FOONFOON_SABRE_SML_MASK_PMAP_ANIM "ship.foonfoon.graphics.sabre.small"
 #define FOONFOON_SML_MASK_PMAP_ANIM "ship.foonfoon.graphics.typhoon.small"
