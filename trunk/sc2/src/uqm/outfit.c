0756a010580519b8b981d8d789b6330352e1317b
Jaakko.Markus.Seppala <Jaakko.Markus.Seppala@79222922-03e5-11de-9d1d-173cc1d06e3c>
2011-11-10 21:18:15 +0000

    Shofixti colony in Beta Gorno I, with preliminary dialogue.

--- trunk/sc2/src/uqm/outfit.c
+++ trunk/sc2/src/uqm/outfit.c
@@ -16,7 +16,7 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-// JMS_GFX 2011: Merged the resolution Factor stuff from UQM-HD.
+// JMS_GFX 2011: Merged the resolution Factor stuff from UQM-HD. 
 
 #include "options.h"
 #include "colors.h"
