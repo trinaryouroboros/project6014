c125059edcc4cfa5cb02c991812c0bf0d181b116
Jaakko.Markus.Seppala <Jaakko.Markus.Seppala@79222922-03e5-11de-9d1d-173cc1d06e3c>
2011-11-11 05:24:49 +0000

    Added the omitted src/uqm/comm/shofixticolony directory.

---
+++ sc2/src/uqm/comm/shofixticolony/strings.h
@@ -0,0 +1,51 @@
+//Copyright Paul Reiche, Fred Ford. 1992-2002
+
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef _STRINGS_H
+#define _STRINGS_H
+
+enum
+{
+	NULL_PHRASE,
+	SHO_GREETING1,
+	SHO_GREETING2,
+	a_woman,
+	YES_WOMAN,
+	i_luv_furries,
+	BACK_OFF_CREEP,
+	you_sound_upset,
+	glad_youre_not_upset,
+	SINCE_YOU_MENTIONED_IT,
+	thats_sad,
+	humans_are_different,
+	SO_DIFFERENT,
+	YOU_UNDERSTAND,
+	gf_at_home,
+	YOURE_JUST_AS_BAD,
+	DIFFERENT_INDEED,
+	just_joking,
+	sorry_disrespectful,
+	JUST_JOKING_REPLY,
+	DISRESPECTFUL_REPLY,
+	tell_me_about_distress_call,
+	DISTRESS_CALL,
+	goodbye,
+	GOODBYE_CAPTAIN
+};
+
+#endif /* _STRINGS_H */
