f53100f82f5b0b28edd81bb12ec823b45effc7b4
benjamin.wack@free.fr <benjamin.wack@free.fr@79222922-03e5-11de-9d1d-173cc1d06e3c>
2011-11-21 16:04:11 +0000

    Fix 4x Earthling comm animations (Transport yet to come) Fix 4x Lurg comm animations Fix Lurg comm crash bug (at least I think I did) : never use exotic fractions of ONE_SECOND for framerates Remove a couple of unused frames from biocritters

--- trunk/sc2/src/uqm/comm/lurg/lurgc.c
+++ trunk/sc2/src/uqm/comm/lurg/lurgc.c
@@ -25,7 +25,7 @@
 #include "uqm/build.h"
 
 
-static LOCDATA lurg_desc =
+static LOCDATA lurg_desc_1x =
 {
 	LURG_CONVERSATION, /* AlienConv */
 	NULL, /* init_encounter_func */
@@ -148,13 +148,13 @@ static LOCDATA lurg_desc =
 			CIRCULAR_ANIM, /* AnimFlags */
 			ONE_SECOND / 19, 0, /* FrameRate */
 			3 * ONE_SECOND / 2, ONE_SECOND / 2, /* RestartRate */
-			(1L << 17), /* BlockMask */
+			(1L << 17) | (1L << 8), /* BlockMask */
 		},
 		{	// 13 - Move lavalamp Right
 			72, /* StartIndex */
 			25, /* NumFrames */
 			YOYO_ANIM, /* AnimFlags */
-			ONE_SECOND / 28, 0, /* FrameRate */
+			ONE_SECOND / 24, 0, /* FrameRate */
 			ONE_SECOND * 2, ONE_SECOND * 5, /* RestartRate */
 			(1L << 0) | (1L << 5) | (1L << 6) | (1L << 10) | (1L << 11) | (1L << 15), /* BlockMask */
 		},
@@ -186,9 +186,9 @@ static LOCDATA lurg_desc =
 			112, /* StartIndex */
 			30, /* NumFrames */
 			YOYO_ANIM, /* AnimFlags */
-			ONE_SECOND / 28, 0, /* FrameRate */
+			ONE_SECOND / 24, 0, /* FrameRate */
 			ONE_SECOND * 2, ONE_SECOND * 3, /* RestartRate */
-			(1L << 0) | (1L << 3) | (1L << 8) | (1L << 12) | (1L << 18), /* BlockMask */
+			(1L << 0) | (1L << 3) | (1L << 8) | (1L << 12) | (1L << 18) | (1L << 19), /* BlockMask */
 		},
 		{	// 18 - Tentacle L2
 			142, /* StartIndex */
@@ -213,6 +213,169 @@ static LOCDATA lurg_desc =
 			ONE_SECOND / 15, 0, /* FrameRate */
 			ONE_SECOND / 2, 3 * ONE_SECOND / 2, /* RestartRate */
 			(1L << 18) | (1L << 19), /* BlockMask */
+		},
+	},
+	{ /* AlienTransitionDesc */
+		1, /* StartIndex */
+		2, /* NumFrames */
+		0, /* AnimFlags */
+		ONE_SECOND / 15, 0, /* FrameRate */
+		0, 0, /* RestartRate */
+		0, /* BlockMask */
+	},
+	{ /* AlienTalkDesc */
+		1, /* StartIndex */
+		4, /* NumFrames */
+		0, /* AnimFlags */
+		ONE_SECOND / 15, 0, /* FrameRate */
+		ONE_SECOND / 12, 0, /* RestartRate */	
+		0, /* BlockMask */
+	},
+	NULL, /* AlienNumberSpeech - none */
+	/* Filler for loaded resources */
+	NULL, NULL, NULL,
+	NULL,
+	NULL,
+	0, /* NumFeatures */
+	{{0, 0, {0}} /*AlienFeatureArray (alternative features) */
+	},
+	{0 /* AlienFeatureChoice (will be computed later) */
+	},
+};
+
+
+static LOCDATA lurg_desc_4x =
+{
+	LURG_CONVERSATION, /* AlienConv */
+	NULL, /* init_encounter_func */
+	NULL, /* post_encounter_func */
+	NULL, /* uninit_encounter_func */
+	LURG_PMAP_ANIM, /* AlienFrame */
+	LURG_FONT, /* AlienFont */
+	WHITE_COLOR_INIT, /* AlienTextFColor */
+	BLACK_COLOR_INIT, /* AlienTextBColor */
+	{0, 0}, /* AlienTextBaseline */
+	0, /* SIS_TEXT_WIDTH - 16, */ /* AlienTextWidth */
+	ALIGN_CENTER, /* AlienTextAlign */
+	VALIGN_BOTTOM, /* AlienTextValign */
+	LURG_COLOR_MAP, /* AlienColorMap */
+	LURG_MUSIC, /* AlienSong */
+	NULL_RESOURCE, /* AlienAltSong */
+	0, /* AlienSongFlags */
+	LURG_CONVERSATION_PHRASES, /* PlayerPhrases */
+	14, /* NumAnimations */
+	{ /* AlienAmbientArray (ambient animations) */
+		{	// 0 - Background throb animation
+			5, /* StartIndex */
+			3, /* NumFrames */
+			YOYO_ANIM | WAIT_TALKING, /* AnimFlags */
+			ONE_SECOND / 10, ONE_SECOND / 10, /* FrameRate */
+			ONE_SECOND, ONE_SECOND * 3, /* RestartRate */
+			(1L << 8) | (1L << 9) | (1L << 10) | (1L << 13), /* BlockMask */
+		},
+		{	// 1 - Eye blink
+			8, /* StartIndex */
+			3, /* NumFrames */
+			YOYO_ANIM, /* AnimFlags */
+			ONE_SECOND / 24, 0, /* FrameRate */
+			ONE_SECOND * 2, ONE_SECOND, /* RestartRate */
+			0, /* BlockMask */
+		},
+		{	// 2 - Spinchter A - Leftmost
+			11, /* StartIndex */
+			2, /* NumFrames */
+			CIRCULAR_ANIM, /* AnimFlags */
+			ONE_SECOND / 15, ONE_SECOND / 15, /* FrameRate */
+			ONE_SECOND, ONE_SECOND * 3, /* RestartRate */
+			0, /* BlockMask */
+		},
+		{	// 3 - Spinchter B - Second left
+			13, /* StartIndex */
+			2, /* NumFrames */
+			CIRCULAR_ANIM, /* AnimFlags */
+			ONE_SECOND / 15, ONE_SECOND / 15, /* FrameRate */
+			ONE_SECOND, ONE_SECOND * 3, /* RestartRate */
+			0, /* BlockMask */
+		},
+		{	// 4 - Spinchter C - Middle left
+			15, /* StartIndex */
+			2, /* NumFrames */
+			CIRCULAR_ANIM, /* AnimFlags */
+			ONE_SECOND / 15, ONE_SECOND / 15, /* FrameRate */
+			ONE_SECOND, ONE_SECOND * 3, /* RestartRate */
+			0, /* BlockMask */
+		},
+		{	// 5 - Spinchter D - Middle right
+			17, /* StartIndex */
+			2, /* NumFrames */
+			CIRCULAR_ANIM, /* AnimFlags */
+			ONE_SECOND / 15, ONE_SECOND / 15, /* FrameRate */
+			ONE_SECOND, ONE_SECOND * 3, /* RestartRate */
+			(1L << 13), /* BlockMask */
+		},
+		{	// 6 - Spinchter E - Second right
+			19, /* StartIndex */
+			2, /* NumFrames */
+			CIRCULAR_ANIM, /* AnimFlags */
+			ONE_SECOND / 15, ONE_SECOND / 15, /* FrameRate */
+			ONE_SECOND, ONE_SECOND * 3, /* RestartRate */
+			(1L << 13), /* BlockMask */
+		},
+		{	// 7 - Spinchter F - Rightmost
+			21, /* StartIndex */
+			2, /* NumFrames */
+			CIRCULAR_ANIM, /* AnimFlags */
+			ONE_SECOND / 15, ONE_SECOND / 15, /* FrameRate */
+			ONE_SECOND, ONE_SECOND * 3, /* RestartRate */
+			0, /* BlockMask */
+		},
+		{	// 8 - Intestine Left
+			23, /* StartIndex */
+			3, /* NumFrames */
+			CIRCULAR_ANIM, /* AnimFlags */
+			ONE_SECOND / 15, 0, /* FrameRate */
+			ONE_SECOND * 2, ONE_SECOND * 3, /* RestartRate */
+			(1L << 0) | (1L << 12), /* BlockMask */
+		},
+		{	// 9 - Intestine Middle
+			26, /* StartIndex */
+			3, /* NumFrames */
+			YOYO_ANIM, /* AnimFlags */
+			ONE_SECOND / 12, 0, /* FrameRate */
+			ONE_SECOND * 2, ONE_SECOND * 5, /* RestartRate */
+			(1L << 0), /* BlockMask */
+		},
+		{	// 10 - Intestine Right
+			29, /* StartIndex */
+			5, /* NumFrames */
+			CIRCULAR_ANIM, /* AnimFlags */
+			ONE_SECOND / 15, 0, /* FrameRate */
+			ONE_SECOND * 3, ONE_SECOND * 3, /* RestartRate */
+			(1L << 0) | (1L << 13), /* BlockMask */
+		},
+		{	// 11 - Lavalamp anim Right
+			34, /* StartIndex */
+			19, /* NumFrames */
+			CIRCULAR_ANIM, /* AnimFlags */
+			ONE_SECOND / 18, 0, /* FrameRate */
+			ONE_SECOND, ONE_SECOND / 2, /* RestartRate */
+			(1L << 13), /* BlockMask */
+		},
+		{	// 12 - Lavalamp anim Left
+			53, /* StartIndex */
+			19, /* NumFrames */
+			CIRCULAR_ANIM, /* AnimFlags */
+			ONE_SECOND / 19, 0, /* FrameRate */
+			3 * ONE_SECOND / 2, ONE_SECOND / 2, /* RestartRate */
+			0, /* BlockMask */
+		},
+		{	// 13 - Move lavalamp Right
+			72, /* StartIndex */
+			25, /* NumFrames */
+			YOYO_ANIM, /* AnimFlags */
+			ONE_SECOND / 24, 0, /* FrameRate */
+			ONE_SECOND * 2, ONE_SECOND * 5, /* RestartRate */
+			(1L << 0) | (1L << 5) | (1L << 6) | (1L << 10) | (1L << 11), /* BlockMask */
 		},
 	},
 	{ /* AlienTransitionDesc */
@@ -417,6 +580,18 @@ LOCDATA*
 init_lurg_comm (void)
 {
 	LOCDATA *retval;
+	static LOCDATA lurg_desc;
+
+	switch (RESOLUTION_FACTOR)
+	{
+	case 2:
+		lurg_desc = lurg_desc_4x;
+		break;
+	case 0:
+	default:
+		lurg_desc = lurg_desc_1x;
+		break;
+	}
 
 	lurg_desc.init_encounter_func = Intro;
 	lurg_desc.post_encounter_func = post_lurg_enc;
