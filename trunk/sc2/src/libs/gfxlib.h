6513b6861f59f013c045a20149ade3f5825e3bd4
Jaakko.Markus.Seppala <Jaakko.Markus.Seppala@79222922-03e5-11de-9d1d-173cc1d06e3c>
2011-11-21 14:41:35 +0000

    Shofixti colony animation implemented.

--- trunk/sc2/src/libs/gfxlib.h
+++ trunk/sc2/src/libs/gfxlib.h
@@ -13,7 +13,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *  Foundation, Inc., 59 Temple Place - SuiCreateDisplayte 330, Boston, MA 02111-1307, USA.
  */
 
 // JMS 2011: Added DPOINT type - a coordinate point with larger values to avoid overflows in hires modes.
