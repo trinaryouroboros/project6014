747c2285719e6f73e5620aa47d0e50483e740778
Jaakko.Markus.Seppala <Jaakko.Markus.Seppala@79222922-03e5-11de-9d1d-173cc1d06e3c>
2011-11-30 18:52:52 +0000

    Centerings for several icons. Fixed bugs in the hint quest for those that 'left in a hurry'.

--- trunk/sc2/src/uqm/planets/generate/genhint2.c
+++ trunk/sc2/src/uqm/planets/generate/genhint2.c
@@ -16,7 +16,7 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-// JMS 2010: - This is a completely new file.
+// JMS 2010: - This is a completely new file which puts a spaceship imprint on a planet near Yehat system.
 
 #include "../../encount.h"
 #include "../../globdata.h"
