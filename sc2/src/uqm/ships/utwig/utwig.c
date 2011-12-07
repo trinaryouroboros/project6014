88b49fba4eafaf16d7b9bc126e7874982937bf56
Jaakko.Markus.Seppala <Jaakko.Markus.Seppala@79222922-03e5-11de-9d1d-173cc1d06e3c>
2011-12-07 12:55:47 +0000

    Yehat and Utwig shields now protect ships from baul gas shockwave (Utwig shield isn't yet reloaded by it.)

--- sc2/src/uqm/ships/utwig/utwig.c
+++ sc2/src/uqm/ships/utwig/utwig.c
@@ -393,7 +393,7 @@ utwig_intelligence (ELEMENT *ShipPtr, EVALUATE_DESC *ObjectsOfConcern,
 				|| (lpEvalDesc->ObjectPtr->state_flags & PLAYER_SHIP) // means IMMEDIATE_WEAPON.
 				|| PlotIntercept (lpEvalDesc->ObjectPtr, ShipPtr, 2, 0))
 				&& (TFB_Random () & 3))
-				&& !(lpEvalDesc->ObjectPtr->state_flags & GASSY_SUBSTANCE))
+				&& (!(lpEvalDesc->ObjectPtr->state_flags & GASSY_SUBSTANCE) || (lpEvalDesc->ObjectPtr->state_flags & GASSY_SUBSTANCE && lpEvalDesc->ObjectPtr->mass_points > 0))) // JMS: means: Don't deflect Baul gas, do deflect Baul spray.
 			{
 				StarShipPtr->ship_input_state |= SPECIAL;
 				StarShipPtr->ship_input_state &= ~WEAPON;
