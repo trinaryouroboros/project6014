fed4c0097d3b70c689bf1530166659c6566343bc
Jaakko.Markus.Seppala <Jaakko.Markus.Seppala@79222922-03e5-11de-9d1d-173cc1d06e3c>
2012-01-09 12:20:27 +0000

    Orz voices. Still unpolished, so expect better versions of the .oggs in the near future.

---
+++ trunk/sc2/content/addons/voice/orz/orz.ts
@@ -0,0 +1,26 @@
+#(ORZ_GREETING)			5350, 3850, 5100
+#(ORZ_GREETING_2)		3600
+#(how_are_things)
+#(HAPPY_ORZ)			4800, 3100, 4600, 2700, 6700, 4600, 4700, 4900, 4000
+#(any_news)
+#(ORZ_NEWS)				4700, 2100, 5800, 5100, 6300
+#(tell_of_nature)
+#(ORZ_TELL_1)			3100, 4900, 4300, 3600, 3700, 5000, 4000, 3300, 5600, 7150
+#(tell_buddies)
+#(ORZ_TELL_2)			6350, 4000, 2250
+#(discuss_alliance_matters)
+#(HAPPY_TO_DISCUSS)		3800, 2400, 2900, 3700, 5200, 1900, 2100
+#(alliance_question_1)
+#(alliance_question_2)
+#(ALLIANCE_ANSWER_1)	3300, 5200, 6800, 5050, 4150, 2700
+#(ALLIANCE_ANSWER_2)	2600
+#(enough_alliance)
+#(ENOUGH_ALLIANCE_OK)	
+#(ask_know_fjorn)
+#(ORZ_DENY_FJORN)
+#(you_have_met_fjorn)
+#(TALK_ABOUT_FJORN)
+#(any_more_news)
+#(ORZ_GARBAGE)			2500, 3500, 3200, 2800, 4000, 3600, 2900, 3500, 4350
+#(goodbye_orz)
+#(GOODBYE_EARTHLING)
